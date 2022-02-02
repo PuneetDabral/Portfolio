30191d242de240e30917e37efcfb3f5d24628c80
Puneet Dabral <puneetdabral2000@gmail.com>
2022-02-02 21:05:16 +0530

    create project page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { projects } from '../data';
+import ProjectCart from '../components/ProjectCart';
 
-const projects = () => {
-  return <div className="p-4">
-      projects
-  </div>;
+
+const Projects = () => {
+  return <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vh'}}>
+    <nav>Navbar</nav>
+      <div className='relative grid grid-cols-12 gap-4 my-3'>
+        {
+          projects.map((project) => (
+            <div className='col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200'>
+             <ProjectCart project={project} key={project.name} />
+            </div>
+
+
+          ))
+        }
+      
+        
+      </div>
+    </div>
+
+      
+  
 };
 
-export default projects;
+export default Projects;
