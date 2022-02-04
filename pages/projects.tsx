c03dbf60f431efc8f133cca559a5a584ee2cec51
Puneet Dabral <puneetdabral2000@gmail.com>
2022-02-05 00:26:34 +0530

    add project-page navbar and its working

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { projects } from '../data';
+import { projects as projectData} from '../data';
 import ProjectCart from '../components/ProjectCart';
+import { ProjectNavbar } from '../components/ProjectNavbar';
+import { useState } from 'react';
+import { Category } from '../types';
 
 
 const Projects = () => {
+  const [projects, setProject] = useState(projectData);
+  const [active, setActive] = useState('all');
+
+  const handlerFilterCategory = (category:Category | 'all') => {
+    if(category==='all'){
+      setProject(projectData);
+      setActive(category); 
+      return;
+    }
+   const newArray = projectData.filter(project=>project.category.includes(category))
+  setProject(newArray);
+  setActive(category);
+  }
   return <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vh'}}>
-    <nav>Navbar</nav>
+  <ProjectNavbar handlerFilterCategory={handlerFilterCategory} active={active}/>
       <div className='relative grid grid-cols-12 gap-4 my-3'>
         {
           projects.map((project) => (
